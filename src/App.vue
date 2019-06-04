515c6ee3bd127438cf22e2867e40a3cf61f46607
Grzegorz <w.luk1972@wp.pl>
2019-06-04 19:27:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <Example>
+  </Example>
+  </div>
+</template>
+
+<script>
+
+import Example from './components/Example.vue';
+export default {
+ name: 'app',  components: {    Example  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Example.vue
@@ -0,0 +1,10 @@
+  <template>
+  <h1>Grzegorz</h1>
+  </template>
+
+  <script>
+    import { exampleMixin } from '../mixins/exampleMixin.js'
+      export default {
+        mixins: [exampleMixin]
+      }
+    </script>
